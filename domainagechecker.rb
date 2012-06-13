d87c7fd594508f4f5430cceade13c455c1de7fe1
Konrads Smelkovs <konrads.smelkovs@kpmg.co.uk>
2012-06-13 09:34:35 +0100

    New functionality + test case: try at most N times before giving up on a domain

--- domainagechecker.rb
+++ domainagechecker.rb
@@ -22,34 +22,59 @@ end
 
 class DomainAgeChecker
 
-   def initialize
+   def initialize (opts={})
+      defaults = {
+         :logger => Logger.new("DomainAgeChecker") ,
+         :delayBetweenRetries => 2,
+         :retries => 5,
+      }
+      @opts = defaults.merge opts
+      @logger=opts[:logger]
+
       @conn = Mongo::Connection.new
       @db = @conn['agechecker']
-      @coll = @db['domains']
+      @coll = @db['domains2']
       @whois = Whois::Client.new
    end
 
    #Get the creation date of a domain and return a hash
    #with domain and creation date (Time)
    #throws DomainAgeCheckerException
-   def getAge(host)
-      return ((Time.now - self.query(host)['created_on'])/ (60*60*24)).round
+   def getAge(host, refTime = nil)
+      if refTime == nil then
+         refTime = Time.now
+      end
+      return ((refTime - self.query(host)['created_on'])/ (60*60*24)).round
    end
+
    def query(host)
       d=Domainatrix.parse "mockfix://#{host}"
       domain="#{d.domain}.#{d.public_suffix}"
       if doc=@coll.find('domain' =>domain).first
          return doc
       end
+
+    w=nil
+    @opts[:retries].times { |i|
+    begin
       w=@whois.query domain
+      break
+    rescue Whois::ResponseIsThrottled,Errno::ECONNRESET  => throttle 
+      if i+1==@opts[:delayBetweenRetries] then
+         raise DomainAgeCheckerException "Giving up after #{@opts[:retries]} attempts"
+      end
+      @logger.debug "Response is throttled, sleeping"
+      sleep @opts[:delayBetweenRetries]
+   end
+    }
       if w.parser.registered?
          if w.created_on
             doc={'domain'=>domain,'created_on'=>w.created_on}
             @coll.insert doc
          else
             errmsg="Could not parse WHOIS record for #{domain}"
-            log.error msg
-            raise DomainAgeCheckerException, msg, :permanent
+            @logger.error errmsg
+            raise DomainAgeCheckerException, errmsg, :permanent
          end
       else
          raise DomainNotFoundException, domain
--- spec/domainagechecker_spec.rb
+++ spec/domainagechecker_spec.rb
@@ -1,4 +1,10 @@
-require_relative 'domainagechecker'
+require_relative '../domainagechecker'
+require 'whois'
+
+RSpec.configure do |config|
+  config.mock_framework = :mocha
+end
+
 GOOGLE_COM_REG_TIME=874278000
 describe DomainAgeChecker, "#query" do
   it "takes both host and domain as input and returns domain age" do
@@ -15,10 +21,21 @@ describe DomainAgeChecker, "#query" do
    }.to raise_error(DomainNotFoundException)
  end
  
+ it "tries N times before giving up" do 
+  saved_query=Whois::Client.new.query("google.com")
+  Whois::Client.any_instance.stubs(:query).
+          raises(Whois::ResponseIsThrottled).then.raises(
+                  Errno::ECONNRESET).then.raises(
+                  Whois::ResponseIsThrottled).then.returns(saved_query)
+  #Whois::Client.any_instance
+   d=DomainAgeChecker.new
+   q=d.query "www.google.com"
+   q['created_on'].to_i.should eq(GOOGLE_COM_REG_TIME)
+ end  
 
-   
  
-end
+ 
+end # DomainAgeChecker#query
 describe DomainAgeChecker, "#getAge" do
  it "returns the number of days since registration as integer" do
     d=DomainAgeChecker.new
