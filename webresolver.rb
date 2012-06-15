97c535b1b852369a460baa128e96ebf894e2125f
Konrads Smelkovs <konrads.smelkovs@kpmg.co.uk>
2012-06-15 14:40:26 +0100

    web resolver returns a friendly greeting

--- webresolver.rb
+++ webresolver.rb
@@ -12,10 +12,10 @@ class WebResolver
     code=200
     if dom=req.GET["domain"]
       begin
-      	res=@resolver.query(dom)
+        res=@resolver.query(dom)
         body=JSON.generate res
       rescue DomainAgeCheckerException =>e
-      	code=500
+        code=500
         body=JSON.generate :exception => e.class , :message => e.message, :type =>e.type
       rescue
         code=500
@@ -24,6 +24,10 @@ class WebResolver
       res = Rack::Response.new("",code,header={'Content-Type'=>'text/json'})
       res.write body
       res.finish
+    else
+    	res=Rack::Response.new("this game has no name",200,header={'Content-Type'=>'text/plain'})
+	    res.finish
     end
+	    
   end
 end
