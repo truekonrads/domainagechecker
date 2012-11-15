d26f72bc7e88c2d067cf8b6cb254d21ad8b8156d
Konrads Smelkovs <konrads.smelkovs@kpmg.co.uk>
2012-11-15 12:03:10 +0000

    working version of dnsreslver

--- dnsresolver.rb
+++ dnsresolver.rb
@@ -1,13 +1,12 @@
-#!/usr/bin/ruby
 # DNS Resolver
 require 'rubygems'
 require 'rubydns'
 require_relative 'domainagechecker'
 require 'trollop'
-
+require 'time'
 $R = Resolv::DNS.new
 SUFFIX="whoislookup.tyrell-corp.co.uk"
-
+require 'pry'
 resolver=DomainAgeChecker.new
 RubyDNS::run_server do
     Name = Resolv::DNS::Name
@@ -15,10 +14,13 @@ RubyDNS::run_server do
 
     match(/^([.\w]+)\.#{SUFFIX}$/, IN::TXT) do |match_data, transaction|
         begin
-        res=resolver.query(dom)
+        res=resolver.query(match_data[1])
+        # binding.pry
         created_on=res['created_on']
-        logger.debug("Responding for #{match_data} with #{created_on}")
-        transaction.respond!(created_on)
+        refTime=Time.now
+        age=((refTime - created_on)/ (60*60*24)).round
+        logger.debug("Responding for #{match_data} with #{age} which was created #{created_on}")
+        transaction.respond!(age.to_s)
       rescue DomainNotFoundException =>e
       	transaction.failure!(:NXDomain)
       	logger.error(e.to_s)
